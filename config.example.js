5b5530e4f796fd544dc6ccfa7ea6a659bd6cd226
yoshizawashinnosuke <yoshizawashinnosuke@team-lab.com>
2020-07-04 14:02:02 +0900

    設定例を追加

---
+++ config.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+  HOST: 'xxx',
+  INTERVAL: 1000,
+
+  // 表示する絵文字毎にレスポンス時間を設定する
+  LEVEL1: 30,   // 😎: SUNGLASSES
+  LEVEL2: 50,   // 😇: INNOCENT
+  LEVEL3: 80,   // 😱: SCREAM
+  LEVEL4: 100,  // 🤢: NAUSEATED_FACE
+  // LEVEL4以上の数値の場合はこれ 👿: IMP
+}
